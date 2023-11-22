8e7f15096f608d26a1a6bbe4525ca879132d26a2
Tiago Gomes <tidgomes@gmail.com>
2023-11-22 09:46:15 -0300

    using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import imgCoreConcepts from './assets/react-core-concepts.png'
+import imgJSXUI from './assets/jsx-ui.png'
+import imgStateMgmt from './assets/state-mgmt.png'
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
 
 function genRandomInt(max) {
@@ -9,7 +13,7 @@ function Header() {
 
   return (
     <header>
-      <img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
+      <img src={imgCoreConcepts} alt='Stylized atom' />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -19,10 +23,37 @@ function Header() {
   )
 }
 
+function CoreConcepts({ imagePath, imgAlt, title, content }) {
+  return (
+    <li>
+      <img src={imagePath} alt={imgAlt} />
+      <h3>{title}</h3>
+      <p>{content}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
+      <section id='core-concepts'>
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts
+            imagePath={imgJSXUI}
+            imgAlt='Image from Mano'
+            title='Mano'
+            content='Mano content'
+          />
+          <CoreConcepts
+            imagePath={imgStateMgmt}
+            imgAlt='Image from Truta'
+            title='Truta'
+            content='Truta content'
+          />
+        </ul>
+      </section>
       <main>
         <h2>Time to get started!</h2>
       </main>
