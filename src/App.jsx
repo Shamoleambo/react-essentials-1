fb86bcf7a9158ffb9d0353c03fd61d44e2ecba8f
Tiago Gomes <tidgomes@gmail.com>
2023-11-28 11:52:10 -0300

    refactor the code separating the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import imgCoreConcepts from './assets/react-core-concepts.png'
 import { CORE_CONCEPTS } from './data'
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
-
-  return (
-    <header>
-      <img src={imgCoreConcepts} alt='Stylized atom' />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcepts({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/Header'
+import CoreConcepts from './components/CoreConcepts'
 
 function App() {
   return (
@@ -40,7 +10,7 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           {CORE_CONCEPTS.map((concept) => (
-            <CoreConcepts {...concept} />
+            <CoreConcepts {...concept} key={concept.title} />
           ))}
         </ul>
       </section>
