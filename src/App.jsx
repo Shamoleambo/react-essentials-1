03324c0f8e5f710d69af785ed3a74f75e9e5a5ae
Tiago Gomes <tidgomes@gmail.com>
2023-11-28 13:02:18 -0300

    create CoreConcepts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data'
 import Header from './components/Header'
 import CoreConcepts from './components/CoreConcepts'
+import TabButton from './components/TabButton'
 
 function App() {
   return (
@@ -15,7 +16,14 @@ function App() {
             ))}
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>??</TabButton>
+            <TabButton>??</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   )
