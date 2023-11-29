ce12d495c38d0e4365dd904761215adc0f305377
Tiago Gomes <tidgomes@gmail.com>
2023-11-29 17:09:41 -0300

    create handleClick function

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,9 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton>Components</TabButton>
-            <TabButton>??</TabButton>
-            <TabButton>??</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
           </menu>
         </section>
       </main>
