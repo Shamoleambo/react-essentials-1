19a10f2839fc185c728c1e870793369d15495c3c
Tiago Gomes <tidgomes@gmail.com>
2023-12-12 14:15:01 -0300

    create Section component

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,15 +1,15 @@
 import { CORE_CONCEPTS } from '../data'
 import CoreConcept from './CoreConcept'
+import Section from './Section'
 
 export default function CoreConcepts() {
   return (
-    <section id='core-concepts'>
-      <h2>Core Concepts</h2>
+    <Section title='Core Concepts'>
       <ul>
         {CORE_CONCEPTS.map((concept) => (
           <CoreConcept {...concept} key={concept.title} />
         ))}
       </ul>
-    </section>
+    </Section>
   )
 }
